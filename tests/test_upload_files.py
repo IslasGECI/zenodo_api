23c6029fd6ebf2218f1162aa8375cf54b576b549
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-10-31 17:36:11 +0000

    🛑🧪 Fail tests

--- tests/test_upload_files.py
+++ tests/test_upload_files.py
@@ -1,8 +1,12 @@
-from zenodo_api.upload_files import call_depositions
+from zenodo_api.upload_files import call_depositions, load_access_token
 
 
 def tests_call_depositions():
     obtained = call_depositions()
     assert obtained.status_code == 403
-
     assert obtained.json()["message"] == "Permission denied."
+
+
+def test_load_access_token():
+    obtained = load_access_token()
+    assert obtained is not None
