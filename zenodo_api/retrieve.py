b535a770ceee0769f5dd8ecb2320ceea14edf7d6
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-11-05 22:02:21 +0000

    ✅ Pass tests

--- zenodo_api/retrieve.py
+++ zenodo_api/retrieve.py
@@ -1,2 +1,11 @@
+import requests
+from zenodo_api.upload_files import load_access_token
+
+
 def retrieve_file(id, id_file):
-    pass
+    ACCESS_TOKEN = load_access_token()
+    response_upload = requests.get(
+        f"https://sandbox.zenodo.org/api/deposit/depositions/{id}/files/{id_file}",
+        [("access_token", ACCESS_TOKEN), ("size", 100)],
+    )
+    return response_upload
