6133fa58f11eadbc0b1991412f373019d0a1538f
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-11-05 22:43:23 +0000

    🛑🧪 Fail tests

--- tests/test_retrieve_files.py
+++ tests/test_retrieve_files.py
@@ -1,8 +1,13 @@
 from zenodo_api.retrieve import retrieve_file
 
+import geci_test_tools as gtt
+
 
 def tests_retrieve_file():
     id = 124630
     id_file = "cf7efbaa-c355-43b1-a4bd-5cc236cce19d"
+    output_file = "paper.pdf"
+    gtt.if_exist_remove(output_file)
     obtained = retrieve_file(id, id_file)
+    gtt.assert_exist(output_file)
     assert obtained.status_code == 200
