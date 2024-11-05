fffef5a3f308f046517778779c3470ca5e37d516
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-11-05 21:56:17 +0000

    🛑🧪 Fail tests

--- tests/test_retrieve_files.py
+++ tests/test_retrieve_files.py
@@ -4,4 +4,5 @@ from zenodo_api.retrieve import retrieve_file
 def tests_retrieve_file():
     id = 124630
     id_file = "cf7efbaa-c355-43b1-a4bd-5cc236cce19d"
-    retrieve_file(id, id_file)
+    obtained = retrieve_file(id, id_file)
+    assert obtained.status_code == 200
