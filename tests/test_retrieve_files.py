01430355ff75293bfb77b97dcaffe844838db268
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-11-07 19:39:26 +0000

    🛑🧪 Fail tests

--- tests/test_retrieve_files.py
+++ tests/test_retrieve_files.py
@@ -1,6 +1,7 @@
-from zenodo_api.retrieve import download_file, download_from_filename
+from zenodo_api.retrieve import download_file, download_from_filename, search_record_by_title
 
 import geci_test_tools as gtt
+import pytest
 
 
 def tests_download_file():
@@ -13,6 +14,7 @@ def tests_download_file():
     gtt.assert_exist(output_file)
 
 
+@pytest.mark.skip(reason="not yet 🪙")
 def tests_download_from_filename():
     id = 124630
     output_file = "China_2004-es.pdf"
@@ -20,3 +22,9 @@ def tests_download_from_filename():
     obtained = download_from_filename(id, output_file)
     assert obtained.status_code == 200
     gtt.assert_exist(output_file)
+
+
+def tests_search_record_by_title():
+    title = "Prueba"
+    obtained = search_record_by_title(title)
+    assert obtained.status_code == 200
