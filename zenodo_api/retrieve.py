ff282355be526de78c6cb7aabc3fbdee60f499f8
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-11-07 17:58:16 +0000

    ♻️  Refactor

--- zenodo_api/retrieve.py
+++ zenodo_api/retrieve.py
@@ -4,16 +4,16 @@ from zenodo_api.upload_files import load_access_token
 
 def retrieve_file(id, id_file):
     ACCESS_TOKEN = load_access_token()
-    response_upload = requests.get(
+    response_info = requests.get(
         f"https://sandbox.zenodo.org/api/deposit/depositions/{id}/files/{id_file}",
         [("access_token", ACCESS_TOKEN), ("size", 100)],
     )
 
-    url = response_upload.json()["links"]["download"]
+    url = response_info.json()["links"]["download"]
 
-    response_upload = requests.get(url)
+    download_response = requests.get(url)
 
     with open("paper.pdf", mode="wb") as file:
-        file.write(response_upload.content)
+        file.write(download_response.content)
 
-    return response_upload
+    return download_response
