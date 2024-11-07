416892aac1632ae8a293d46807ed38d8871f957e
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-11-07 18:16:47 +0000

    ♻️  Refactor

--- zenodo_api/retrieve.py
+++ zenodo_api/retrieve.py
@@ -3,13 +3,7 @@ from zenodo_api.upload_files import load_access_token
 
 
 def retrieve_file(id, id_file):
-    ACCESS_TOKEN = load_access_token()
-    response_info = requests.get(
-        f"https://sandbox.zenodo.org/api/deposit/depositions/{id}/files/{id_file}",
-        [("access_token", ACCESS_TOKEN), ("size", 100)],
-    )
-
-    url = response_info.json()["links"]["download"]
+    url = get_download_url(id, id_file)
 
     download_response = requests.get(url)
 
@@ -17,3 +11,14 @@ def retrieve_file(id, id_file):
         file.write(download_response.content)
 
     return download_response
+
+
+def get_download_url(id, id_file):
+    ACCESS_TOKEN = load_access_token()
+    response_info = requests.get(
+        f"https://sandbox.zenodo.org/api/deposit/depositions/{id}/files/{id_file}",
+        [("access_token", ACCESS_TOKEN), ("size", 100)],
+    )
+
+    url = response_info.json()["links"]["download"]
+    return url
