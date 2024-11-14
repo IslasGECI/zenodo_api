af466be92d8ea485e311f4be8b6c64c64547496a
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-11-14 19:44:38 +0000

    🛑🧪 Fail tests

--- tests/test_url_selector.py
+++ tests/test_url_selector.py
@@ -5,3 +5,7 @@ def test_url_selector():
     obtained = url_selector()
     expected = "https://sandbox.zenodo.org/api"
     assert obtained == expected
+
+    obtained = url_selector(tests=False)
+    expected = "https://zenodo.org/api"
+    assert obtained == expected
