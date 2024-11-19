fe9172181c60b611a0b52846de15371c00da5838
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-11-19 22:23:24 +0000

    🛑🧪 Fail tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,6 +4,16 @@ from typer.testing import CliRunner
 runner = CliRunner()
 
 
+def test_download_from_geci_zenodo():
+    result = runner.invoke(cli, ["download-from-geci-zenodo", "--help"])
+    assert result.exit_code == 0
+    assert " download-from-geci-zenodo " in result.stdout
+
+
 def test_version():
+    result = runner.invoke(cli, ["--help"])
+    assert result.exit_code == 0
+    assert " version " in result.stdout
+
     result = runner.invoke(cli, ["version", "--help"])
     assert result.exit_code == 0
