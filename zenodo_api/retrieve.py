1d163e9e778e11d6cd9b4e12d61b59c8880dc397
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-11-21 19:02:00 +0000

    ♻️  Refactor

--- zenodo_api/retrieve.py
+++ zenodo_api/retrieve.py
@@ -21,14 +21,6 @@ def search_by_doi(doi, url_api):
     return response_info
 
 
-def download_file_by_id_and_organization(id, organization, is_sandbox):
-    url_api = url_selector(tests=is_sandbox)
-    response_info = search_record_by_two_parameters(id, organization, url_api)
-    extracted_id = extract_record_id_and_file_id(response_info.json())
-    downloaded_file = download_file(extracted_id["record_id"], extracted_id["file_id"], url_api)
-    return downloaded_file
-
-
 def download_file_by_doi(doi, is_sandbox):
     url_api = url_selector(tests=is_sandbox)
     response_info = search_by_doi(doi, url_api)
