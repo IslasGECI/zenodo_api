c57f98dfa52e00ed0d88ef166716fe2bd081bbb2
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2024-11-21 17:37:47 +0000

    ✅ Pass tests

--- zenodo_api/__init__.py
+++ zenodo_api/__init__.py
@@ -4,4 +4,4 @@ __version__ = "0.0.1"
 from .upload_files import *  # noqa
 from .url_selector import *  # noqa
 from .retrieve import *  # noqa
-from .cli import cli
+from .cli import cli  # noqa
--- zenodo_api/cli.py
+++ zenodo_api/cli.py
@@ -1,4 +1,5 @@
 import typer
+from zenodo_api.retrieve import download_file_by_id_and_organization
 
 cli = typer.Typer()
 
@@ -12,4 +13,5 @@ def version():
 def download_from_geci_zenodo(
     doi: str = typer.Option(), is_sandbox: str = typer.Option(help="Output file path")
 ):
-    pass
+    organization = "Grupo de Ecología y Conservación de Islas"
+    download_file_by_id_and_organization(doi, organization)
