cc0c57f7a66ba5e150354044036406c538ed1a29
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-04-07 17:21:10 +0000

    ♻️  Refactor

--- tests/test_new_version.py
+++ tests/test_new_version.py
@@ -4,4 +4,6 @@ from zenodo_api.new_version import get_latest_version
 def test_get_latest_version():
     concept_rec_id = "131633"
     obtained = get_latest_version(concept_rec_id)
+
     assert 200 <= obtained.status_code < 300
+    assert obtained.json()["id"] == 137021
