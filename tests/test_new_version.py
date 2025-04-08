9aec8309a1865a4dd69d0726079e903137ed41fc
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-04-08 16:36:35 +0000

    🛑🧪 Fail tests

--- tests/test_new_version.py
+++ tests/test_new_version.py
@@ -1,8 +1,15 @@
-from zenodo_api.new_version import get_latest_version_id
+from zenodo_api.new_version import create_draft_of_new_version, get_latest_version_id
 
 
-def test_get_latest_version():
+def test_get_latest_version_id():
     concept_rec_id = "131633"
     obtained = get_latest_version_id(concept_rec_id)
 
     assert obtained == 137021
+
+
+def test_create_draft_of_new_version():
+    latest_version_id = 137021
+    obtained = create_draft_of_new_version(latest_version_id)
+
+    assert 200 <= obtained.status_code < 300
