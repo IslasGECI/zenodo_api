95ecfb2d91058ca6fe09ffd5e14efa1598e90d37
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-04-09 18:43:00 +0000

    ✅ Pass tests

--- zenodo_api/upload_files.py
+++ zenodo_api/upload_files.py
@@ -34,10 +34,12 @@ def upload_file_in_new_record(file_path):
     empty_upload = create_deposition_in_new_record()
 
     bucket_url = empty_upload.json()["links"]["bucket"]
-    print(empty_upload.json())
 
     response_upload = upload_file(params, bucket_url, file_path)
-    return {"response_upload": response_upload}
+    return {
+        "response_upload": response_upload,
+        "latest_draft": empty_upload.json()["links"]["latest_draft"],
+    }
 
 
 def upload_file(params, bucket_url, file_path):
