32ab586264bfba77d794a1d4c2fb5791e9cabd8f
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-04-15 17:27:15 +0000

    ♻️  Refactor

--- tests/test_new_version.py
+++ tests/test_new_version.py
@@ -42,3 +42,6 @@ def test_upload_file_in_new_version():
     obtained = upload_file_in_new_version(latest_version_id, file_path)
     print(obtained.json(), "upload file in new version")
     assert obtained.status_code == 201
+    time.sleep(1)
+    latest_draft_link = search_deposition_by_title(title, is_sandbox=True)
+    requests.delete(latest_draft_link, params={"access_token": access_token})
