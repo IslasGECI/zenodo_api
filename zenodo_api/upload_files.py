c0c4e5182f8e1a7394bf1f241679e61974d85fdb
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-04-16 18:28:17 +0000

    ♻️  Refactor

--- zenodo_api/upload_files.py
+++ zenodo_api/upload_files.py
@@ -60,11 +60,12 @@ def upload_metadata_in_new_record(data_dict):
 
 
 def upload_metadata(data_dict, deposition_id):
-    data_dict["metadata"].update({"version": "X.Y.Z"})
-    data_dict["metadata"].update(
-        {"creators": [{"name": "Grupo de Ecología y Conservación de Islas"}]}
-    )
-    data_dict["metadata"].update({"access_right": "restricted"})
+    default_metadata = {
+        "version": "X.Y.Z",
+        "creators": [{"name": "Grupo de Ecología y Conservación de Islas"}],
+        "access_right": "restricted",
+    }
+    data_dict["metadata"].update(default_metadata)
     headers = {"Authorization": f"Bearer {load_access_token()}", "Content-Type": "application/json"}
     response = requests.put(
         f"https://sandbox.zenodo.org/api/deposit/depositions/{deposition_id}",
