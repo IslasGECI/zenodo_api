8e6721044bd3a17d8c9d2c8838bd3b37c386b818
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-04-29 17:04:29 +0000

    ♻️  Refactor

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,17 +1,21 @@
 from zenodo_api import cli
 from typer.testing import CliRunner
 import geci_test_tools as gtt
-
+import re
 
 runner = CliRunner()
+
+
+def strip_ansi_sequences(text):
+    ansi_escape = re.compile(r"\x1b\[([0-9;]*[mGKF])")
+    return ansi_escape.sub("", text)
 
 
 def test_download_from_geci_zenodo():
     result = runner.invoke(cli, ["download-from-geci-zenodo", "--help"], env={"CLICOLOR": "0"})
     assert result.exit_code == 0
-    print("result.stdout:", result.stdout)
-    assert " --is-sandbox " in result.stdout
-    assert " --doi " in result.stdout
+    assert " --is-sandbox " in strip_ansi_sequences(result.stdout)
+    assert " --doi " in strip_ansi_sequences(result.stdout)
 
     output_file = "tests_file.txt"
     doi = "10.5072/zenodo.131633"
@@ -26,7 +30,7 @@ def test_download_from_geci_zenodo():
 def test_version():
     result = runner.invoke(cli, ["--help"])
     assert result.exit_code == 0
-    assert " version " in result.stdout
+    assert " version " in strip_ansi_sequences(result.stdout)
 
     result = runner.invoke(cli, ["version", "--help"])
     assert result.exit_code == 0
