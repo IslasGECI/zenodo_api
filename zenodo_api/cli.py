fd72aa092c07786db030fc36369616c623a72cdf
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-04-29 17:37:52 +0000

    ✅ Pass tests

--- zenodo_api/cli.py
+++ zenodo_api/cli.py
@@ -14,3 +14,8 @@ def download_from_geci_zenodo(
     doi: str = typer.Option(), is_sandbox: bool = typer.Option(False, "--is-sandbox")
 ):
     download_file_by_doi(doi, is_sandbox)
+
+
+@cli.command()
+def publish_to_geci_zenodo():
+    pass
