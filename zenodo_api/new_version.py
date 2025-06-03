8aa995a31921d08a8becbcaa8aba73c0aa81c10e
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-06-03 02:05:14 -0700

    ♻️  Refactor

--- zenodo_api/new_version.py
+++ zenodo_api/new_version.py
@@ -27,7 +27,8 @@ def open_new_deposition(concept_rec_id, is_test):
 
 
 def upload_file_in_new_deposition(new_deposition_json, file_path):
-    upload_new_deposition_metadata(new_deposition_json)
+    is_sandbox = True
+    xxupload_new_deposition_metadata(new_deposition_json, is_sandbox)
     delete_previous_files(new_deposition_json)
     bucket_url = new_deposition_json["links"]["bucket"]
     response = upload_file(bucket_url, file_path)
@@ -55,10 +56,10 @@ def create_draft_of_new_version(latest_version_id, is_sandbox):
     return response
 
 
-def upload_new_deposition_metadata(new_deposition_json):
+def xxupload_new_deposition_metadata(new_deposition_json, is_sandbox):
     new_deposition_id = new_deposition_json["id"]
     previous_metadata = new_deposition_json["metadata"]
-    upload_metadata(previous_metadata, new_deposition_id, is_sandbox=True)
+    upload_metadata(previous_metadata, new_deposition_id, is_sandbox)
 
 
 def delete_previous_files(new_deposition_json):
